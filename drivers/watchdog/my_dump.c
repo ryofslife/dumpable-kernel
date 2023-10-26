fdbc94c4f431ba4fbc23cee0f1a34be112607ebb
Ryo Takakura <fsryo75@gmail.com>
2023-10-26 21:56:54 +0900

    turn led on

--- drivers/watchdog/my_dump.c
+++ drivers/watchdog/my_dump.c
@@ -1,16 +1,29 @@
 #include <linux/init.h> 
 #include <linux/module.h> 
 #include <linux/printk.h>
+#include <linux/gpio.h>
+
+#define GPIO_PIN_LED 16
 
 static int __init dump_init(void) 
 { 
-    printk("dump_init: Hello world\n"); 
+
+    printk("dump_init: Hello world\n");
+
+    // LEDを点灯する 
+    gpio_set_value(GPIO_PIN_LED, 1);
+
     return 0; 
 } 
 
 static void __exit dump_exit(void) 
 { 
-    printk("dump_exit: Hello world\n"); 
+
+    printk("dump_exit: Bye\n"); 
+
+    // LEDを消灯する
+    gpio_set_value(GPIO_PIN_LED, 0);
+
 } 
 
 module_init(dump_init); 
